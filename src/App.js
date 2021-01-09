cacc5455a582088c5412a87eeaea0bf0d8532dd1
Okiki Oriola <okikioriola1@gmail.com>
2021-01-09 15:02:41 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+
+import './App.css';
+import Navbar from './components/Navbar';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Home from './components/pages/HomePage/Home'
+import Services from './components/pages/Services/Services'
+import Footer from './components/pages/Folder/Footer';
+
+function App() {
+  return (
+    <Router>
+      <Navbar/>
+      <Switch>
+        <Route path='/' exact component={Home}/>
+        <Route path='/services'  component={Services}/>
+      </Switch>
+      <Footer/>
+    </Router>
+  );
+}
+
+export default App;
